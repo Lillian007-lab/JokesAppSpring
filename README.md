26a1d72e80558620fe0724caa33db5e8bee7c2b0
Carol Zhou <zcarol.530@gmail.com>
2021-05-17 23:43:23 -0400

    init spring boot

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JokesAppSpring
+
 Chuck Norris Jokes - Spring MVC
---
+++ src/test/java/com/example/jokesappspring/JokesAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jokesappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
